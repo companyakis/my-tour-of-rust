29ba438c2bb3eeab0fcb36dd577ccf26f228b11e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-07 22:29:53 +0300

    Create 0006-Constants.rs

---
+++ 0006-Constants.rs
@@ -0,0 +1,10 @@
+// Unlike variables, constants must always have explicit types!
+
+const FOUNDER: &str = "Mustafa Büyükdereli";
+const PI_NUMBER: f32 = 3.14;
+
+fn main() {
+
+    println!("Founder is {FOUNDER} and pi number: {PI_NUMBER}") // Founder is Mustafa Büyükdereli and pi number: 3.14
+
+}
