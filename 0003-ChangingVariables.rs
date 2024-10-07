80d1e3e41bc328796244ffc3219dc8a8c8e04b05
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-07 14:26:42 +0300

    Create 0003-ChangingVariables.rs

---
+++ 0003-ChangingVariables.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    let mut her_age = 19;
+    
+    println!("She is {her_age}");
+    
+    her_age = 34;
+    
+    println!("The correct age => She is {}", her_age)
+}
+
+// She is 19
+// The correct age => She is 34
