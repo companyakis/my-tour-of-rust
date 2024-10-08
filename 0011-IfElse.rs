09d1f78c6ed27045d01cc1486ac60f1b411bd39c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-08 08:52:07 +0300

    Create 0011-IfElse.rs

---
+++ 0011-IfElse.rs
@@ -0,0 +1,20 @@
+fn main() {
+
+    // ==, !=, <, >, <=, >=, !, ||, &&
+
+    let x = 14;
+    
+    let y = 9;
+    
+    if x != y {
+        
+        println!("They are not equal!")
+    }
+    
+    else {
+        
+        println!("They are equal.")
+    }
+
+}
+
