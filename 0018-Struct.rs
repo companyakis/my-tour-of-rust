ceebf701682c01e878dcfd81493aea36da1d20bd
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-09 13:33:50 +0300

    Create 0018-Struct.rs

---
+++ 0018-Struct.rs
@@ -0,0 +1,11 @@
+struct Person {
+    
+    name: String,
+    last_name: String,
+    birth_year: u16,
+    city: String,
+    country: String,
+    is_married: bool,
+    has_children: bool,
+  
+}
